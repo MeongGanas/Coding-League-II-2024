9953b934485b7d89cb545cce7119aeac12c7e09c
idanfath <arfounded@gmail.com>
2024-10-23 13:10:15 +0800

    masyarakat done

--- app/Http/Controllers/MasyarakatController.php
+++ app/Http/Controllers/MasyarakatController.php
@@ -40,7 +40,7 @@ class MasyarakatController extends Controller
         $mitra = Mitra::where('status', '!=', 'Pengajuan');
         $laporan = Laporan::where('status', 'Diterima');
         $dashboardFunction = new DashboardController();
-        $dashboardFunction->applyFilters($proyek, $mitra, $laporan);
+        $dashboardFunction->applyFilters(clone $proyek, clone $mitra, clone $laporan);
 
         return Inertia::render('Masyarakat/Statistik', [
             'counts' => [
@@ -50,9 +50,9 @@ class MasyarakatController extends Controller
                 'countTotalDanaRealized' => $laporan->sum('realisasi'),
             ],
             'realisasi' => [
-                'dataCSR' => $dashboardFunction->getRealisasiBy($laporan, 'sektor_id', 'sektor', 'name', true, null, true, true)->values(),
-                'persenTotalMitra' => $dashboardFunction->getRealisasiBy($laporan, 'mitra_id', 'mitra', 'name', null, null, true, true)->values(),
-                'persenTotalKecamatan' => $dashboardFunction->getRealisasiBy($laporan, 'lokasi', 'kecamatan', 'lokasi', null, null, true)->values(),
+                'dataCSR' => $dashboardFunction->getRealisasiBy(clone $laporan, 'sektor_id', 'sektor', 'name', true, null, true, true)->values(),
+                'persenTotalMitra' => $dashboardFunction->getRealisasiBy(clone $laporan, 'mitra_id', 'mitra', 'name', null, null, true, true)->values(),
+                'persenTotalKecamatan' => $dashboardFunction->getRealisasiBy(clone $laporan, 'lokasi', 'kecamatan', 'lokasi', null, null, true)->values(),
             ],
             'filters' => [
                 'tahun' => $dashboardFunction->getPossibleYear(clone $proyek, clone $mitra, clone $laporan)->values(),
